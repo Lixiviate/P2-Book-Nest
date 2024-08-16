7941426528ce90901257853a775c36618c4b3a98
Kenneth A Wagner <kenw2190@gmail.com>
2024-08-15 21:37:45 -0500

    created user model, schema.sql file. Working on password hashing and seeds data tomorrow.

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,6 @@
-DROP DATABASE IF EXISTS blog_db;
-CREATE DATABASE blog_db;
+\c postgres;
+
+DROP DATABASE IF EXISTS bookNest_db;
+CREATE DATABASE bookNest_db;
+
+\c bookNest_db;
--- models/Book.js
+++ models/Book.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// Create a new Sequelize model for books
 class Book extends Model {}
 
 Book.init(
