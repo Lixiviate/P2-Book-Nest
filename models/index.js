e7b59e183f7d4aa1533fd78aa4ae5350212a35ff
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-08-17 20:49:51 -0600

    Clean up routes, add boilerplate to handlebars

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const User = require('./User');
 const Book = require('./Book');
 const Holder = require('./Holder');
 
+// User/Book association (Ownership)
 User.hasMany(Book, {
   foreignKey: 'user_id',
 });
@@ -10,12 +11,22 @@ Book.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
+// Holder/Book association
 Holder.belongsTo(Book, {
   foreignKey: 'book_id',
 });
 
 Book.hasOne(Holder, {
   foreignKey: 'book_id',
+});
+
+// User/Holder association (Borrowing)
+Holder.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Holder, {
+  foreignKey: 'user_id',
 });
 
 module.exports = { User, Book, Holder };
