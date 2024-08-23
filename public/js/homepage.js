51435b6231d8d97e32defdc35d59f7de7ba708db
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-08-22 20:15:23 -0500

    Added the safety warning modal and a justLoggedIn session to userRoutes

---
+++ public/js/homepage.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const safetyModal = document.getElementById('safety-modal');
+  safetyModal.style.display = 'block';
+
+  const closeButton = document.getElementById('close-modal');
+  closeButton.addEventListener('click', function () {
+    safetyModal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function (event) {
+    if (event.target == safetyModal) {
+      safetyModal.style.display = 'none';
+    }
+  });
+});
