0af6acef18297315793d66bd4ffff2efb3300616
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-08-25 19:10:29 -0500

    Created a Wishlist model

---
+++ models/Wishlist.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Wishlist extends Model {}
+
+Wishlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    book_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'book',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'wishlist',
+  },
+);
+
+module.exports = Wishlist;
